be997eb220749e6265e5e5f0bb474bd3842ec2ad
hson@ncsu.edu <hson@ncsu.edu>
2022-02-06 01:06:32 -0500

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  resources :friends
+  #get 'home/index'
+  get  'home/about'
+  root 'home#index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
